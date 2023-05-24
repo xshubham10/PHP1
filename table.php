1c6ef014f919c904a241812bc0d21b4360e7e4c0
Shubham Majumdar <shubham.majumdar@ssipmt.com>
2023-05-24 18:29:12 +0530

    table

---
+++ table.php
@@ -0,0 +1,6 @@
+<?php
+    $num = 5; // Change this to the number you want the multiplication table for
+    for ($i = 1; $i <= 10; $i++) {
+        echo $num . " * " . $i . " = " . ($num * $i) . "\n";
+    }
+?>
